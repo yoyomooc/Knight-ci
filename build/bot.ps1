91c547cd51f9e48e9f2c8967724ce090c2c19518
werltm <werltm@hotmailcom>
2024-06-16 13:53:16 +0800

    init

---
+++ build/bot.ps1
@@ -0,0 +1,53 @@
+
+param(
+    [string]$BotUrl,
+    [string]$Msg
+)
+
+$currentDate = (Get-Date)
+$currentDateStr = $currentDate.ToString('yyyy-MM-dd HH:mm:ss')
+
+$rootPath = Split-Path -Parent (Get-Location).Path
+
+# 读取当前项目配置
+$ciConfigPath = Join-Path $rootPath "src" "ci-config.json"
+$ciConfig = (Get-Content -Path $ciConfigPath -Encoding UTF8) | ConvertFrom-Json
+
+$branchOrTagKey = 'branch'
+if ($ciConfig.mode -eq 'tag') {
+    $branchOrTagKey = 'tag'
+}
+$branchOrTag = $ciConfig.branch
+$commit = $ciConfig.commit
+$gitlabPipelineId = $ciConfig.gitlabPipelineId
+
+$workflowUrl = "https://github.com/${env:repository}/actions/runs/${env:run_id}"
+$pipelineUrl = "${env:GIT_REPO_PIPLINE}/${gitlabPipelineId}"
+
+$noticeMsg = @"
+--- ${currentDateStr} ---
+${Msg}
+
+--- build info ---
+${branchOrTagKey}: ${branchOrTag}
+commit: ${commit}
+github workflow: ${workflowUrl}
+gitlab pipeline: ${pipelineUrl}
+"@
+
+
+# ----------------- 发送通知 -----------------
+$body = @{
+    msg_type = 'text'
+    content  = @{
+        text = $noticeMsg
+    }
+}
+
+$bodyJson = ConvertTo-Json $body
+
+Invoke-RestMethod `
+    -Method 'Post' `
+    -ContentType 'application/json' `
+    -Uri $botUrl `
+    -Body $bodyJson
---
+++ build/clone-target-repo.ps1
@@ -0,0 +1,49 @@
+# 仓库地址
+$RepoUrl = $env:GIT_REPO
+
+# 顶级目录
+$rootPath = Split-Path -Parent (Get-Location).Path
+
+# 读取当前项目配置
+$ciConfigPath = Join-Path $rootPath "src" "ci-config.json"
+$ciConfig = (Get-Content -Path $ciConfigPath -Encoding UTF8) | ConvertFrom-Json
+
+
+# 设置环境变量
+[Environment]::SetEnvironmentVariable("TAG", $ciConfig.branch, "Machine")
+[Environment]::SetEnvironmentVariable("TAG", $ciConfig.branch)
+Write-Host "${env:TAG}"
+
+
+# 克隆目标仓库代码
+## git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
+if ($ciConfig.mode -eq 'commit') {
+    git clone --depth 1 `
+        -b $ciConfig.branch `
+        $RepoUrl repo-code
+    Set-Location ./repo-code
+    git log
+    git checkout $ciConfig.commit
+    Set-Location ..
+    # 切换到目标仓库代码
+    Set-Location repo-code/build
+}
+if ($ciConfig.mode -eq 'tag') {
+    git clone --depth 1 `
+        --branch $ciConfig.branch `
+        $RepoUrl repo-code
+    Set-Location ./repo-code
+    git log
+    git checkout $ciConfig.commit
+    Set-Location ..
+    # 切换到目标仓库代码
+    Set-Location repo-code/build
+}
+
+# 执行错误判断
+if ($Error.Count -eq 0) {
+    exit 0
+}
+else {
+    exit 1
+}
